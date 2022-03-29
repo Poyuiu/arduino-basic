0b32b2e2ba7921db0ce8d2ac68f7467a1c19acfa
poyu <yoyohihirobert@gmail.com>
2022-03-29 16:48:22 +0800

    week6InClass complete

--- week6InClass/week6InClass.ino
+++ week6InClass/week6InClass.ino
@@ -30,13 +30,36 @@ void setup() {
   pinMode(motorL2, OUTPUT);
   pinMode(motorR1, OUTPUT);
   pinMode(motorR2, OUTPUT);
+
+  pinMode(trigL, OUTPUT);
+  pinMode(trigR, OUTPUT);
+  pinMode(echoL, INPUT);
+  pinMode(echoR, INPUT);
 }
 
 void loop() {
   irlstate = digitalRead(irl);
   irrstate = digitalRead(irr);
+  ultrasoundL();
+  ultrasoundR();
   if (irlstate == 1 && irrstate == 1) {
-    straightWithSpeed2();
+    if (distanceR >= 20 && distanceL >= 20) {
+      straightWithSpeed3();
+    } else if (distanceR >= 10 && distanceR < 20) {
+      leftWithSpeed1();
+    } else if (distanceL >= 10 && distanceL < 20) {
+      rightWithSpeed1();
+    } else if (distanceR >= 5 && distanceR < 10) {
+      leftWithSpeed2();
+    } else if (distanceL >= 5 && distanceL < 10) {
+      rightWithSpeed2();
+    } else if (distanceR < 5) {
+      leftWithSpeed3();
+    } else if (distanceL < 5) {
+      rightWithSpeed3();
+    } else {
+      straightWithSpeed3();
+    }	 
   }
   else if (irrstate == 1) {
     leftWithSpeed2();
